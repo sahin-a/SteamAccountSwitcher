e65ab1179d9daadff22d77649cc66965a14175e0
sar <sahin.aral@outlook.de>
2021-12-01 17:06:36 +0100

    di project

---
+++ DI/Dependencies.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using SteamAccountManager.Infrastructure;
+
+namespace DI
+{
+    public static class Dependencies
+    {
+        public static void RegisterModules(this ContainerBuilder builder)
+        {
+            builder.RegisterInfrastructureModule();
+        }
+    }
+}
