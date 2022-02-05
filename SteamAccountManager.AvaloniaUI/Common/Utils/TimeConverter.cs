7a1cee896e30932a3da4a6e0746eb7108a6813f0
sar <sahin.aral@outlook.de>
2022-02-05 19:16:30 +0100

    int overflow fixed

--- SteamAccountManager.AvaloniaUI/Common/Utils/TimeConverter.cs
+++ SteamAccountManager.AvaloniaUI/Common/Utils/TimeConverter.cs
@@ -2,8 +2,8 @@
 {
     public static class TimeConverter
     {
-        public static long ToDays(int minutes) => minutes / Time.DAY_IN_MINUTES;
+        public static long ToDays(long minutes) => minutes / Time.DAY_IN_MINUTES;
 
-        public static long ToHours(int minutes) => minutes / Time.HOUR_IN_MINUTES;
+        public static long ToHours(long minutes) => minutes / Time.HOUR_IN_MINUTES;
     }
 }
