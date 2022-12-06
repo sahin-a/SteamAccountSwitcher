dbaacb6d805fc315bdb7aa8860410eb574fb0559
sahin-a <>
2022-12-06 19:48:25 +0100

    service impl

---
+++ SteamAccountManager.Domain/Steam/Service/ISteamService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SteamAccountManager.Domain.Steam.Local.POCO;
+
+namespace SteamAccountManager.Domain.Steam.Service
+{
+    public interface ISteamService
+    {
+        public Task<List<SteamLoginUser>> GetAccounts();
+        public bool LogInAccount(string accountName);
+    }
+}
---
+++ SteamAccountManager.Infrastructure/Steam/Service/SteamService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SteamAccountManager.Domain.Steam.Local.POCO;
+using SteamAccountManager.Domain.Steam.Local.Repository;
+using SteamAccountManager.Domain.Steam.Service;
+
+namespace SteamAccountManager.Infrastructure.Steam.Service
+{
+    public class SteamService : ISteamService
+    {
+        private readonly ISteamRepository _steamRepository;
+
+        public SteamService(ISteamRepository steamRepository)
+        {
+            _steamRepository = steamRepository;
+        }
+
+        public async Task<List<SteamLoginUser>> GetAccounts()
+        {
+            try
+            {
+                return await _steamRepository.GetSteamLoginUsers();
+            }
+            catch (Exception e)
+            {
+                return new List<SteamLoginUser>();
+            }
+        }
+
+        public bool LogInAccount(string accountName)
+        {
+            try
+            {
+                _steamRepository.UpdateAutoLoginUser(accountName);
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
