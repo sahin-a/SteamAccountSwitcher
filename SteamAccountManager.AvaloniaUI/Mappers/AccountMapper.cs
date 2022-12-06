e5d15a198f0ea48ad5b954f7d12771ff3ba76fe3
sahin-a <>
2022-12-06 19:48:51 +0100

    moved account mapping

---
+++ SteamAccountManager.AvaloniaUI/Mappers/AccountMapper.cs
@@ -0,0 +1,67 @@
+﻿using SteamAccountManager.Application.Steam.Model;
+using SteamAccountManager.AvaloniaUI.Common;
+using SteamAccountManager.AvaloniaUI.Common.Utils;
+using SteamAccountManager.AvaloniaUI.Models;
+using SteamAccountManager.AvaloniaUI.Services;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamAccountManager.AvaloniaUI.Mappers
+{
+    internal class AccountMapper
+    {
+        private AvatarService _avatarService;
+
+        public AccountMapper(AvatarService avatarService)
+        {
+            _avatarService = avatarService;
+        }
+
+        private string GetTimePassedFormatted(int minutesPassed)
+        {
+            var stringBuilder = new StringBuilder();
+
+            switch (minutesPassed)
+            {
+                case >= Time.DAY_IN_MINUTES:
+                    stringBuilder.Append($"{TimeConverter.ToDays(minutesPassed)} days");
+                    break;
+                case >= Time.HOUR_IN_MINUTES:
+                    stringBuilder.Append($"{TimeConverter.ToHours(minutesPassed)} hours");
+                    break;
+                default:
+                    stringBuilder.Append($"{minutesPassed} minutes");
+                    break;
+            }
+            stringBuilder.Append(" ago");
+
+            return stringBuilder.ToString();
+        }
+
+        public async Task<Account> FromSteamAccount(SteamAccount steamAccount)
+        {
+            var minutesPassed = Convert.ToUInt16(DateTime.UtcNow.Subtract(steamAccount.LastLogin).TotalMinutes);
+            var timePassedSinceLastLogin = GetTimePassedFormatted(minutesPassed);
+            var rank = new Rank
+            {
+                Level = steamAccount.Level
+            };
+
+            var account = new Account
+            {
+                SteamId = steamAccount.SteamId,
+                Name = steamAccount.AccountName,
+                ProfilePicture = await _avatarService.GetAvatarAsync(steamAccount.AvatarUrl),
+                Username = steamAccount.Username,
+                ProfileUrl = steamAccount.ProfileUrl,
+                IsVacBanned = steamAccount.IsVacBanned,
+                IsCommunityBanned = steamAccount.IsCommunityBanned,
+                LastLogin = timePassedSinceLastLogin,
+                Rank = rank
+            };
+
+            return account;
+        }
+    }
+}
--- SteamAccountManager.AvaloniaUI/ViewModels/AccountSwitcherViewModel.cs
+++ SteamAccountManager.AvaloniaUI/ViewModels/AccountSwitcherViewModel.cs
@@ -1,15 +1,11 @@
 ﻿using SteamAccountManager.Application.Steam.Model;
 using SteamAccountManager.Application.Steam.Service;
-using SteamAccountManager.AvaloniaUI.Common;
-using SteamAccountManager.AvaloniaUI.Common.Utils;
+using SteamAccountManager.AvaloniaUI.Mappers;
 using SteamAccountManager.AvaloniaUI.Models;
-using SteamAccountManager.AvaloniaUI.Services;
 using SteamAccountManager.AvaloniaUI.ViewModels.Commands;
-using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
@@ -19,16 +15,19 @@ namespace SteamAccountManager.AvaloniaUI.ViewModels
     internal class AccountSwitcherViewModel
     {
         private ISteamService _steamService;
+        private AccountMapper _accountMapper;
+
         public ObservableCollection<Account> Accounts { get; }
         public ICommand ProfileClickedCommand { get; }
         public ICommand RefreshAccountsCommand { get; }
         public ICommand ShowInfoCommand { get; }
-        private AvatarService _avatarService;
 
-        public AccountSwitcherViewModel(ISteamService steamService, AvatarService imageProvider)
+
+        public AccountSwitcherViewModel(ISteamService steamService, AccountMapper accountMapper)
         {
             _steamService = steamService;
-            _avatarService = imageProvider;
+            _accountMapper = accountMapper;
+
             Accounts = new ObservableCollection<Account>();
             ProfileClickedCommand = new ProfileClickedCommand();
             RefreshAccountsCommand = new QuickCommand(LoadAccounts);
@@ -37,62 +36,13 @@ namespace SteamAccountManager.AvaloniaUI.ViewModels
             LoadAccounts();
         }
 
-        public string GetTimePassedFormatted(int minutesPassed)
-        {
-
-            var stringBuilder = new StringBuilder();
-
-            switch (minutesPassed)
-            {
-                case >= Time.DAY_IN_MINUTES:
-                    stringBuilder.Append($"{TimeConverter.ToDays(minutesPassed)} days");
-                    break;
-                case >= Time.HOUR_IN_MINUTES:
-                    stringBuilder.Append($"{TimeConverter.ToHours(minutesPassed)} hours");
-                    break;
-                default:
-                    stringBuilder.Append($"{minutesPassed} minutes");
-                    break;
-            }
-            stringBuilder.Append(" ago");
-
-            return stringBuilder.ToString();
-        }
-
-        // TODO: get it out of this class
-        public async Task<Account> ToAccount(SteamAccount steamAccount)
-        {
-            var minutesPassed = Convert.ToUInt16(DateTime.UtcNow.Subtract(steamAccount.LastLogin).TotalMinutes);
-            var timePassedSinceLastLogin = GetTimePassedFormatted(minutesPassed);
-            var rank = new Rank
-            {
-                Level = steamAccount.Level
-            };
-
-            var account = new Account
-            {
-                SteamId = steamAccount.SteamId,
-                Name = steamAccount.AccountName,
-                ProfilePicture = await _avatarService.GetAvatarAsync(steamAccount.AvatarUrl),
-                Username = steamAccount.Username,
-                ProfileUrl = steamAccount.ProfileUrl,
-                IsVacBanned = steamAccount.IsVacBanned,
-                IsCommunityBanned = steamAccount.IsCommunityBanned,
-                LastLogin = timePassedSinceLastLogin,
-                Rank = rank
-            };
-
-            return account;
-        }
-
         public async void LoadAccounts()
         {
             List<SteamAccount> steamAccounts = await _steamService.GetAccounts();
-            Account[] accounts = await Task.WhenAll(steamAccounts.ConvertAll(x => ToAccount(x)));
+            Account[] accounts = await Task.WhenAll(steamAccounts.ConvertAll(x => _accountMapper.FromSteamAccount(x)));
 
             foreach (var account in accounts)
             {
-                // update entry if already exists
                 var currentAccount = Accounts.Where(x => x.SteamId == account.SteamId).FirstOrDefault();
                 if (currentAccount is Account)
                 {
