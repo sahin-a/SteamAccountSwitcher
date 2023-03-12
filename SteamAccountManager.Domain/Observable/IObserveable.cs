4e88614612b8b95b90875a8be55a09f04bba7f08
sar <sahin.aral@outlook.de>
2023-03-12 16:08:44 +0100

    removed obsolete observable  stuff

--- SteamAccountManager.Domain/Observable/IObserveable.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SteamAccountManager.Domain.Observable
-{
-    public interface IObserveable<T>
-    {
-        public IDisposable Subscribe(Action<T?> observeable);
-        public void Notify(T? value);
-    }
-}
