efbab71269503497e81e9b77cf38409b32febc77
sahin-a <sahin.aral@outlook.de>
2023-04-01 00:18:08 +0200

    added key value data source interface

---
+++ SteamAccountManager.Infrastructure/Steam/Local/DataSource/IKeyValueDataSource.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace SteamAccountManager.Infrastructure.Steam.Local.DataSource;
+
+public interface IKeyValueDataSource
+{
+    public Task<bool> Store(string key, string value);
+    public Task<string?> Load(string key);
+}
