a29fa8205e04d3ec3ad0f3737d3b54733293c6e2
sahin-a <sahin.aral@outlook.de>
2023-04-07 04:42:14 +0200

    added app data directory registry

---
+++ SteamAccountManager.Infrastructure/Steam/Local/Storage/AppDataDirectory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace SteamAccountManager.Infrastructure.Steam.Local.Storage;
+
+static class AppDataDirectory
+{
+    private static string createDirectory(string directory) =>
+        Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+            "sahin-a",
+            "Steam Account Switcher",
+            directory
+        );
+
+    public static readonly string Configurations = createDirectory("Configurations");
+    public static readonly string Storages = createDirectory("Storages");
+    public static readonly string Logs = createDirectory("Logs");
+}
