f163077d35f09b392e3ab79609d75efffed1a133
sahin-a <sahin.aral@outlook.de>
2023-04-07 22:50:13 +0200

    switched from inheritance to composition

--- SteamAccountManager.Infrastructure/Steam/Local/Storage/LocalKeyValueStorage.cs
+++ SteamAccountManager.Infrastructure/Steam/Local/Storage/LocalKeyValueStorage.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using SteamAccountManager.Domain.Steam.Local.Logger;
 using SteamAccountManager.Infrastructure.Steam.Local.Dao;
@@ -6,32 +7,42 @@ using SteamAccountManager.Infrastructure.Steam.Local.DataSource;
 
 namespace SteamAccountManager.Infrastructure.Steam.Local.Storage
 {
-    public abstract class LocalKeyValueStorage<TValue> : ObjectStorage<Dictionary<string, TValue>>,
-        IKeyValueStorage<TValue> where TValue : class
+    public abstract class LocalKeyValueStorage<TValue> : IKeyValueStorage<TValue> where TValue : class
     {
+        private readonly SemaphoreSlim semaphoreSlim = new(1, 1);
+        private readonly ObjectStorage<Dictionary<string, TValue>> _storage;
+
         protected LocalKeyValueStorage(ILogger logger, IFileProvider fileProvider, string name) : this(logger,
             new FileDataSource(directory: AppDataDirectory.Storages, fileProvider), name)
         {
         }
 
         private LocalKeyValueStorage(ILogger logger, IKeyValueDataSource dataSource,
-            string name) : base(name, logger, dataSource)
+            string name)
         {
+            _storage = new ObjectStorage<Dictionary<string, TValue>>(name, logger, dataSource);
         }
-
-        protected override Dictionary<string, TValue> GetDefaultValue() => new();
 
         public async Task Store(string key, TValue value)
         {
-            var avatarMap = await base.Get();
-            avatarMap![key] = value;
+            await semaphoreSlim.WaitAsync();
 
-            await Set(avatarMap);
+            try
+            {
+                var avatarMap = await _storage.Get();
+                avatarMap![key] = value;
+
+                await _storage.Set(avatarMap);
+            }
+            finally
+            {
+                semaphoreSlim.Release();
+            }
         }
 
         public async Task<TValue?> Get(string key, TValue? defaultValue = null)
         {
-            var avatarMap = await base.Get();
+            var avatarMap = await _storage.Get();
             return avatarMap!.TryGetValue(key, out var value) ? value : defaultValue;
         }
     }
